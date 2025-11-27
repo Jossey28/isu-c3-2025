b4e64a488f82a0a4e500f0c5bf30595d86a1cf1c
Irene A <121013355+Jossey28@users.noreply.github.com>
2025-11-26 22:23:50 -0600

    Fixed error controller.

--- news-source/src/main/java/cdc4/newsapi/CustomErrorController.java
+++ news-source/src/main/java/cdc4/newsapi/CustomErrorController.java
@@ -1,10 +1,10 @@
 package cdc4.newsapi;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Error {
+@Controller
+public class CustomErrorController {
     @GetMapping("/error")
     public String handleError() {
         System.out.println("You shouldn't be here; an error has occurred.");
