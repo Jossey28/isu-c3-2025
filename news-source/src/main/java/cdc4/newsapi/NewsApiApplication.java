6ffe8f5b5627460c49d170e218113342624b6a28
78nineball <78nineball@gmail.com>
2025-12-06 13:17:22 -0600

    more attempts

--- news-source/src/main/java/cdc4/newsapi/NewsApiApplication.java
+++ news-source/src/main/java/cdc4/newsapi/NewsApiApplication.java
@@ -2,10 +2,18 @@ package cdc4.newsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class NewsApiApplication {
+
+   private static final Logger logger = LoggerFactory.getLogger(NewsApiApplication.class)
+
    public static void main(String[] args) {
       SpringApplication.run(NewsApiApplication.class, args);
+
+      //test log line
+      logger.info("Application started successfully, Log4j2 is active");
    }
 }
